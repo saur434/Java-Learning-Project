23c85993c86c9d548230a50cb42016a717e040e8
sndhande <saurabh.dhande@oracle.com>
2026-06-09 18:46:19 +0530

    Generic Class Example

---
+++ src/Generic_Class.java
@@ -0,0 +1,10 @@
+public class Generic_Class<T> {
+    T value;
+    public T getData()
+    {
+        return value;
+    }
+    public void setValue(T value){
+        this.value = value;
+    }
+}
